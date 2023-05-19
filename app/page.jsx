93351ec498421ee2c545f4997089830907abd9ba
SShipon <nxshipon@gmail.com>
2023-05-19 10:32:21 +0600

    main dote css

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h2 className='text-9xl text-center'>pages</h2>
+        <div className=' text-center my-10 mx-10'>
+            <h1 className='text-xs p-10'>this is first code xs</h1>
+            <h2 className='text-sm p-10'>this is first code</h2>
+            <h3 className='text-base p-10'>this is first code</h3>
+            <h4 className='text-xl p-10'>this is first code</h4>
+            <h5 className='text-md p-10'>this is first code</h5>
+            <h6 className='text-2xl p-10'>this is first code</h6>
+            <h6 className='text-2xl p-10'>this is first code</h6>
+            <h6 className='text-3xl p-10'>this is first code</h6>
+            <h6 className='text-4xl p-10'>this is first code</h6>
+            <h6 className='text-5xl p-10'>this is first code</h6>
+            <h6 className='text-6xl p-10'>this is first code</h6>
+            <h6 className='text-7xl p-10'>this is first code</h6>
+            <h6 className='text-8xl p-10'>this is first code</h6>
+            <h6 className='text-9xl p-10'>this is first code</h6>
         </div>
     );
 };
